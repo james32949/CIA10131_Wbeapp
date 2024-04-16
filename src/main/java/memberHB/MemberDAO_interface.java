b5695624e5ed1b6e87860f231a8035c2096e9db8
james <dh91009@gmail.com>
2024-04-17 07:04:01 +0800

    commit4/17

--- src/main/java/memberHB/MemberDAO_interface.java
+++ src/main/java/memberHB/MemberDAO_interface.java
@@ -8,7 +8,6 @@ public interface MemberDAO_interface {
 	public void update(MemberVO memberVO);
 	public void upimage(MemberVO memberVO);
 	public void delete(Integer memberId);
-	public void upimage(MemberVO memberVO);
 	public MemberVO findByPrimaryKey(Integer memberId);
 	public List<MemberVO> getAll();
 }
